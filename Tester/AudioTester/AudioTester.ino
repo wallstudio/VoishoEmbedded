4330a42c137b9c9f56b24ac235a8b2601a36efdf
wallstudio <com.haru.31+wallstudio@gmail.com>
2017-04-30 22:29:54 +0900

    Change Pin assign hardwere

--- Tester/AudioTester/AudioTester.ino
+++ Tester/AudioTester/AudioTester.ino
@@ -4,16 +4,21 @@
 void setup() {
   // put your setup code here, to run once:
         Serial.begin (9600);
-        pinMode(INPUT,14);
-        digitalWrite(14,HIGH);
+        pinMode(INPUT,2);
+        pinMode(INPUT,15);
+        digitalWrite(2,HIGH);
+        digitalWrite(15,LOW);
  mp3_set_serial(Serial);
   delay(1);
   mp3_set_volume(30);
 }
 
+bool a = true;
 void loop() {
   // put your main code here, to run repeatedly:
-        digitalWrite(14,!digitalRead(14));
+  a=!a;
+        //digitalWrite(2,a);
+        //digitalWrite(15,a);
 mp3_play(3);
-delay(1000);
+delay(5000);
 }
--- Tester/WatchdogsChecker/WatchdogsChecker.ino
+++ Tester/WatchdogsChecker/WatchdogsChecker.ino
@@ -6,13 +6,19 @@ int i=0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,OUTPUT);
-  digitalWrite(2,LOW);
-  delay(5000);
-  wdt_enable(WDTO_8S);
+  for(int i=0;i<12;i++){
+    digitalWrite(2,HIGH);
+    delay(50);
+    digitalWrite(2,LOW);
+    delay(50);
+  }
   digitalWrite(2,HIGH);
+  delay(1000);
+  wdt_enable(WDTO_4S);
+  digitalWrite(2,LOW);
   set_sleep_mode(SLEEP_MODE_STANDBY);
   sleep_mode();
-  delay(10000);
+  delay(100);
 }
 void loop() {
   // put your main code here, to run repeatedly:
